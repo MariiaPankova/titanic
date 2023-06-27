6ca171aad1445c736be75fb336cbc0641cb8dfcd
mariiapankova <pankova.maa@gmail.com>
2023-06-27 17:35:09 +0300

    formatted, added README

--- src/train.py
+++ src/train.py
@@ -2,8 +2,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split, cross_val_score, cross_validate, KFold
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import (
+    cross_validate,
+    KFold,
+)
 import pandas as pd
 from dvclive import Live
 from dvc.api import params_show
@@ -11,19 +13,43 @@ from rich import print
 
 RANDOM_STATE = 42
 
+
 def get_model(model_params: dict):
-    pipeline = Pipeline([
-        ("column_transformer", ColumnTransformer([
-            ("Pclass-onehot", OneHotEncoder(handle_unknown='ignore'),["Pclass"]),
-            ("Sex-onehot", OneHotEncoder(handle_unknown='ignore'), ["Sex"]),
-            ("Cabin-onehot", OneHotEncoder(handle_unknown='ignore'), ["Cabin"]),
-            ("Embarked-onehot", OneHotEncoder(handle_unknown='ignore'), ["Embarked"])
-        ])),
-        ("estimator", RandomForestClassifier(**model_params, random_state=RANDOM_STATE))
-    ]) 
+    pipeline = Pipeline(
+        [
+            (
+                "column_transformer",
+                ColumnTransformer(
+                    [
+                        (
+                            "Pclass-onehot",
+                            OneHotEncoder(handle_unknown="ignore"),
+                            ["Pclass"],
+                        ),
+                        ("Sex-onehot", OneHotEncoder(handle_unknown="ignore"), ["Sex"]),
+                        (
+                            "Cabin-onehot",
+                            OneHotEncoder(handle_unknown="ignore"),
+                            ["Cabin"],
+                        ),
+                        (
+                            "Embarked-onehot",
+                            OneHotEncoder(handle_unknown="ignore"),
+                            ["Embarked"],
+                        ),
+                    ]
+                ),
+            ),
+            (
+                "estimator",
+                RandomForestClassifier(**model_params, random_state=RANDOM_STATE),
+            ),
+        ]
+    )
     return pipeline
 
-def main(df_path: str="data/train.csv"):
+
+def main(df_path: str = "data/train.csv"):
     params = params_show()
     print(params)
     model = get_model(params["model"])
@@ -31,12 +57,18 @@ def main(df_path: str="data/train.csv"):
     X = titanic_data.drop(["Survived", "Name", "Ticket"], axis="columns")
     Y = titanic_data["Survived"]
     cv = KFold(n_splits=5, random_state=RANDOM_STATE, shuffle=True)
-    score = cross_validate(model, X, Y, cv=cv, scoring={'acc': 'accuracy', 'presision': 'precision', "rec": "recall"})
+    score = cross_validate(
+        model,
+        X,
+        Y,
+        cv=cv,
+        scoring={"acc": "accuracy", "presision": "precision", "rec": "recall"},
+    )
     with Live(save_dvc_exp=True) as live:
-        for k,v in score.items():
-            live.log_metric(f'{k}_mean', v.mean())
+        for k, v in score.items():
+            live.log_metric(f"{k}_mean", v.mean())
             live.log_metric(f"{k}_std", v.std())
-    
+
 
 if __name__ == "__main__":
     main()
